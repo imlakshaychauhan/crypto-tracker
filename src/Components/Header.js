36ff29ade93a45313866f4d8baecebb70f179226
lakshay chauhan <lakshaychauhan030@gmail.com>
2022-06-01 12:05:53 +0530

    Header Done

---
+++ src/Components/Header.js
@@ -0,0 +1,74 @@
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Select,
+  IconButton,
+  MenuIcon,
+  makeStyles,
+  createTheme,
+  ThemeProvider,
+  MenuItem,
+} from "@material-ui/core";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    color: "gold",
+    flex: 1,
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+    type: "dark",
+  },
+});
+const Header = () => {
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const {currency, setCurrency} = CryptoState();
+  console.log(currency);
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <AppBar color="transparent" position="static">
+          <Container>
+            <Toolbar>
+              <Typography
+                className={classes.title}
+                onClick={() => navigate("/")}
+                variant="h6"
+              >
+                Crypto Tracker
+              </Typography>
+              <Select
+                variant="outlined"
+                style={{
+                  width: 100,
+                  height: 40,
+                  marginLeft: 15,
+                }}
+                value = {currency}
+                onChange = {(e) => setCurrency(e.target.value)}
+              >
+                <MenuItem value={"USD"}> USD </MenuItem>
+                <MenuItem value={"INR"}> INR </MenuItem>
+              </Select> 
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default Header;
